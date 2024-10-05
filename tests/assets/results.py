213febe65a8ae837de40d65cb3cb6e6e7fa3e8d1
AlexHorta <buey.horta@gmail.com>
2024-10-05 15:10:19 -0300

    Change the basic format of paths file

--- tests/assets/results.py
+++ tests/assets/results.py
@@ -6,12 +6,11 @@ basic = {'meta': {'title': 'Document title',
 			'content': {'stuff': 'Example text.\n'}}
 
 
-test_paths = {'about': [{'applications': {}, 'quotes': {}, 'getting_started': {}}], 
-				'downloads': [{'all_releases': {}, 'source_code': {}, 'windows': {}, 
-				'mac_os': {}, 'other_platforms': {}}], 'documentation': [
-				{'beginners_guide': {}, 'developers_guide': {}, 'faq': {}}], 
-				'community': [{'mailing_lists': {}, 'forums': {}, 
-				'conferences': {}}], 'news': {}}
+test_paths = {'about': {'applications': {}, 'quotes': {}, 'getting_started': {}}, 
+			'downloads': {'all_releases': {}, 'source_code': {}, 'windows': {}, 
+			'mac_os': {}, 'other_platforms': {}}, 'documentation': {'beginners_guide': {}, 
+			'developers_guide': {}, 'faq': {}}, 'community': {'mailing_lists': {}, 
+			'forums': {}, 'conferences': {}}, 'news': {}}
 
 
 test_basic_paths = {'about': [{'applications': {}, 'getting_started': {}}]}
