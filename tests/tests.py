9cf84a096f6732641c0b000baa8bfac73c3c17e1
AlexHorta <buey.horta@gmail.com>
2024-10-07 23:03:24 -0300

    Remove cruft

--- tests/tests.py
+++ tests/tests.py
@@ -11,21 +11,6 @@ from src import ssg
 assets = "tests/assets"
 
 
-# class TestReader(unittest.TestCase):
-
-	# def test_reader(self):
-	# 	self.assertEqual(ssg.reader(os.path.join(assets, "basic.toml")), 
-	# 		results.basic)
-
-	# def test_wrong_post_format(self):
-	# 	"""Check if there's meta and content tables."""
-	# 	with self.assertRaises(KeyError):
-	# 		ssg.reader(os.path.join(assets, "TestReader/wrong_meta.toml"))
-	# 		ssg.reader(os.path.join(assets, "TestReader/no_meta.toml"))
-	# 		ssg.reader(os.path.join(assets, "TestReader/wrong_content.toml"))
-	# 		ssg.reader(os.path.join(assets, "TestReader/no_content.toml"))
-
-
 class TestPost(unittest.TestCase):
 
 	def test_reader(self):
