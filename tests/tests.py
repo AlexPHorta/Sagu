8fc1444f0033eef2648d01c6750cbc7ebd79c178
AlexHorta <buey.horta@gmail.com>
2024-10-12 21:40:52 -0300

    Renaming some variables

--- tests/tests.py
+++ tests/tests.py
@@ -46,55 +46,56 @@ class TestPost(unittest.TestCase):
 
 	def test_post_with_path(self):
 		"""A post with a path defined will be tested against the website's paths."""
-		paths = ssg.Library(asset("basic_paths.toml"))
+		library = ssg.Library(asset("basic_paths.toml"))
 		post1 = ssg.Post(asset("simple_ok_post.toml"))
-		self.assertTrue(post1.path in paths.flat_tree)
+		self.assertTrue(post1.path in library.flat_tree)
 
 	def test_post_with_wrong_parent_path(self):
 		"""A post with a wrong path will trigger an exception."""
-		paths = ssg.Library(asset("basic_paths.toml"))
+		library = ssg.Library(asset("basic_paths.toml"))
 		with self.assertRaises(KeyError):
-			ssg.Post(asset("wrong_parent_path.toml"), website_path=paths.flat_tree)
+			ssg.Post(asset("wrong_parent_path.toml"), website_path=library.flat_tree)
 
 	def test_post_content_markdown(self):
 		"""Retrieve a post with markdown content."""
-		paths = ssg.Library(asset("basic_paths.toml"))
+		library = ssg.Library(asset("basic_paths.toml"))
 		post = ssg.Post(asset("simple_ok_post_with_markdown_content.toml"), 
-							  website_path=paths.flat_tree)
+							  website_path=library.flat_tree)
 		self.assertEqual(post.content, results.content_markdown)
+
 
 class TestLibrary(unittest.TestCase):
 
 	def test_empty_library(self):
 		"""The library of posts."""
-		posts = ssg.Library()
-		self.assertEqual(posts.size, 0)
-		self.assertEqual(posts.flat_tree, None)
+		library = ssg.Library()
+		self.assertEqual(library.size, 0)
+		self.assertEqual(library.flat_tree, None)
 
 	def test_empty_library_with_path(self):
 		"""The website map is defined in a toml file."""
-		paths = ssg.Library(asset("TestLibrary/paths.toml"))
-		self.assertEqual(paths.flat_tree, results.test_flat_paths)
+		library = ssg.Library(asset("TestLibrary/paths.toml"))
+		self.assertEqual(library.flat_tree, results.test_flat_paths)
 
 	def test_library_with_path_and_posts(self):
 		"""Posts added to the library will be tested against the paths when added."""
-		posts = ssg.Library(asset("basic_paths.toml"))
+		library = ssg.Library(asset("basic_paths.toml"))
 		cases = {"simple_ok_post.toml": ('about:applications', 
 							'073032467b1bffb192b560d04f9b0192'),
 				 "simple_ok_alternative_post.toml": ('about:getting_started', 
 				 			'87ce9d57e6f1a53a887e4834b9d620e0')}
 		for case, attrs in cases.items():
-			post = ssg.Post(asset(case), website_path=posts.flat_tree)
-			posts.add_post(post)
-			self.assertEqual(posts.flat_tree[attrs[0]], {attrs[1]: post})
-		self.assertEqual(posts.size, 2)
+			post = ssg.Post(asset(case), website_path=library.flat_tree)
+			library.add_post(post)
+			self.assertEqual(library.flat_tree[attrs[0]], {attrs[1]: post})
+		self.assertEqual(library.size, 2)
 
 	def test_library_retrieve_post_content(self):
 		"""The library will manage the posts, so, retrieving a post's content is the library's function."""
-		posts = ssg.Library(asset("basic_paths.toml"))
-		post = ssg.Post(asset("simple_ok_post.toml"), website_path=posts.flat_tree)
-		posts.add_post(post)
-		self.assertEqual(posts.get_post(post.id), {'id':post.id, 
+		library = ssg.Library(asset("basic_paths.toml"))
+		post = ssg.Post(asset("simple_ok_post.toml"), website_path=library.flat_tree)
+		library.add_post(post)
+		self.assertEqual(library.get_post(post.id), {'id':post.id, 
 						 'title':post.title, 'content':post.content})
 
 
@@ -112,17 +113,23 @@ class TestBuilder(unittest.TestCase):
 		self.assertTrue(builder.env.autoescape)
 
 	def test_builder_build_post(self):
-		posts = ssg.Library(asset("basic_paths.toml"))
+		library = ssg.Library(asset("basic_paths.toml"))
 		post = ssg.Post(asset("TestBuilder/index.toml"), 
-						website_path=posts.flat_tree)
-		posts.add_post(post)
+						website_path=library.flat_tree)
+		library.add_post(post)
 		builder = ssg.Builder(asset("TestBuilder/"))
 		template = builder.env.get_template("index.jinja")
 		with open(asset("TestBuilder/index.html")) as f:
 			# using html.unescape just to ease the comparison
 			self.assertEqual(html.unescape(
-							 template.render(posts.get_post(post.id))), 
+							 template.render(library.get_post(post.id))), 
 							 f.read().strip())
+
+class TestOrganizer(unittest.TestCase):
+
+	def test_organizer(self):
+		library = ssg.Library(asset("basic_paths.toml"))
+		organizer = ssg.Organizer(library.tree)
 
 
 if __name__ == '__main__':
