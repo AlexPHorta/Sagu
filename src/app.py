42c833fe3b6fd9a176de9b0e049397451523fb80
AlexHorta <buey.horta@gmail.com>
2024-10-28 23:05:55 -0300

    Website creation wizard

--- src/app.py
+++ src/app.py
@@ -7,19 +7,35 @@ GREETING = """Bento's static site generator!
 Answer the following questions to create the necessary files.
 """
 
-DEFAULT_SETTINGS = ['.', ]
 
 def create_project():
     print(GREETING)
-    answers = []
 
-    if f:=input("> Where do you want to create your website? "
-        "[Default: This same folder (.)] ") == '':
-        answers.append('.')
-    else:
-        answers.append(f)
+    default_settings = {
+        "main_directory": '.',
+        "website_title": '',
+        "website_author": '',
+        "website_language": 'en',
+        "website_url": '',
+        "website_timezone": 'Europe/Rome',
+    }
 
-    answers.append(input("> What's the website's title? "))
+    if f:=input(f"> Where do you want to create your website? [Default: '{default_settings["main_directory"]}'] ") != '':
+        default_settings["main_directory"] = f
+
+    default_settings["website_title"] = input("> What's the website's title? ")
+    default_settings["website_author"] = input("> What's the author's name? ")
+
+    if f:=input(f"> What's the default language of the website? [Default: {default_settings["website_language"]}]") == '':
+        default_settings["website_language"] = f
+
+    default_settings["website_url"] = input("> What will be the website's URL "
+        "(e.g. https://example.com)? [Default: empty]")
+
+    if f:=input(f"> What's your timezone? [Default: {default_settings["website_timezone"]}]") == '':
+        default_settings["website_timezone"] = f
+
+    return default_settings
 
 
 def parse_args(args):
@@ -30,7 +46,7 @@ def parse_args(args):
 
 
 if __name__ == "__main__":
-    
+
     args = parse_args(sys.argv[1:])
 
     if args.create:
