8765f89a695b15f837096ae7fffaa272a6d59613
AlexHorta <buey.horta@gmail.com>
2025-02-03 09:55:31 -0300

    Prepare to test full page output

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -16,7 +16,7 @@ class TestBuilder:
                     "SITETIMEZONE": "Europe/Rome",
                     "THEME": "sagu",
                     "BASETEMPLATE": "index.jinja",
-                    "STATIC": "static",
+                    "THEME_STATIC": "static",
                     "MENUITEMS": [
                         { "href": "animals/dogs/the-noble-akita.html",
                           "title": "Akita" },
@@ -29,10 +29,21 @@ class TestBuilder:
                     }
         return settings
 
-    def test_builder(self, capsys, mock_settings):
-        """The builder will manage the mixing of posts and templates."""
+    @pytest.fixture
+    def basic_mock_settings(self, mock_settings):
         mock_settings["BASETEMPLATE"] = "basic.jinja"
-        _builder = builder.Builder(asset("TestBuilder/"), mock_settings)
+        return mock_settings
+
+    @pytest.fixture
+    def full_mock_settings(self, mock_settings):
+        mock_settings["SITENAME"] = "Cats and Dogs"
+        mock_settings["BASETEMPLATE"] = "full.jinja"
+        return mock_settings
+
+
+    def test_builder(self, capsys, basic_mock_settings):
+        """The builder will manage the mixing of posts and templates."""
+        _builder = builder.Builder(asset("TestBuilder/"), basic_mock_settings)
         template = _builder.template
         assert template.render(name="Test") == "Hello, Test!\n"
 
@@ -51,12 +62,11 @@ class TestBuilder:
             test_builder_post = _library.get_post_by_id(_post.id)
             assert template.render(test_builder_post.get_contents()) == f.read()
     
-    def test_builder_build_post_complete(self, mock_settings):
-        mock_settings["BASETEMPLATE"] = "full.jinja"
+    def test_builder_build_post_complete(self, full_mock_settings):
         _library = library.Library(asset("TestBuilder/full/paths.toml"))
         _post = post.Post(asset("TestBuilder/full/full.toml"), website_path=_library.flat_tree)
         _library.add_post(_post)
-        _builder = builder.Builder(asset("TestBuilder/full/"), mock_settings)
+        _builder = builder.Builder(asset("TestBuilder/full/"), full_mock_settings)
         template = _builder.template
         with open(asset("TestBuilder/full/full.html")) as f:
             test_builder_post = _library.get_post_by_id(_post.id)
